0ea6fa3b321d3f7f93f9b2149525a66d58113314
Dennis Snell <dennis.snell@automattic.com>
2015-11-08 12:34:37 -0700

    Clean up some rough edges for publication

--- lib/post.jsx
+++ lib/post.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { decode } from 'ent';
 import moment from 'moment';
 
 import { getPost } from 'postStore';
@@ -26,7 +27,7 @@ export default React.createClass( {
 				<Link to="/">Back</Link>
 				{ post ?
 					<div className="post-content">
-						<h2>{ post.title }</h2>
+						<h2>{ decode( post.title ) }</h2>
 						<h3>Posted by { post.author.name } on { moment( post.date ).format( 'MMMM D' ) }</h3>
 						<div dangerouslySetInnerHTML={ { __html: post.content } } />
 					</div>
